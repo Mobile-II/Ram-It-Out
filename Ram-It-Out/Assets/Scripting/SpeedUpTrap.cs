6599c8a6a9f00f673dd7a3e5483a1574c4a1bae6
William8500 <62640433+William8500@users.noreply.github.com>
2021-03-11 20:19:39 +0800

    William

--- Ram-It-Out/Assets/Scripting/SpeedUpTrap.cs
+++ Ram-It-Out/Assets/Scripting/SpeedUpTrap.cs
@@ -14,7 +14,7 @@ public class SpeedUpTrap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Boxes = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
