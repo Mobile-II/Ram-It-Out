830c6853ca860948f029730555b95c65e35380dc
csi36869 <chaikimlim@gmail.com>
2021-04-13 17:35:30 +0800

    Improved player jump and push rotate
    Updated PlayerMovement.cs.
    Added CharacterController component to player.
    Remove player's rigidbody.

--- Ram-It-Out/Assets/Scripting/PlayerMovement.cs
+++ Ram-It-Out/Assets/Scripting/PlayerMovement.cs
@@ -6,19 +6,26 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody playerRB;      // Player RigidBody
+    //public Rigidbody playerRB;      // Player RigidBody
     public Text textFile;           // Get UI Text
     public bool pushingBox;         // Enable to pull or push
-    bool jumpingLimit;              // Trigger jumping timer
+    //bool jumpingLimit;              // Trigger jumping timer
     public bool pushingActive;      // Trigger to when near box
     bool jumpActive;
     int jumpCount;                  // Current jump count
-    int jumpMax = 1;                // Maximum jump count
+    int jumpMax = 2;                // Maximum jump count
     float limitTime;                // Jumping timer
     public GameObject buttonJump;
     public GameObject buttonPush;
     public GameObject buttonRelease;
 
+    CharacterController charCtrl;
+    private Vector3 moveDirection = Vector3.zero;
+    public float jumpSpeed = 8.0f;
+    public float gravityValue = 16.0f;
+    public float doubleJumpMultiplier = 1.2f;
+    public float moveSpeed = 3.5f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -29,56 +36,73 @@ public class PlayerMovement : MonoBehaviour
         buttonRelease.GetComponent<Image>().enabled = false;
         buttonPush.SetActive(false);
         buttonPush.GetComponent<Image>().enabled = false;
-        
+
         //Disable all the boolean for pushing box
         //Enable the jump action in initial
-        jumpingLimit = false;
+        //jumpingLimit = false;
         pushingActive = false;
         pushingBox = false;
         jumpCount = jumpMax;
         limitTime = 0f;
-        
+
         //Fixing player rigidbody when start the game
-        playerRB = GetComponent<Rigidbody>();
-        playerRB.constraints = RigidbodyConstraints.FreezePositionX |
-                               RigidbodyConstraints.FreezeRotationX |
-                               RigidbodyConstraints.FreezeRotationY |
-                               RigidbodyConstraints.FreezeRotationZ;
+        //playerRB = GetComponent<Rigidbody>();
+        //playerRB.constraints = RigidbodyConstraints.FreezePositionX |
+        //RigidbodyConstraints.FreezeRotationX |
+        //RigidbodyConstraints.FreezeRotationY |
+        //RigidbodyConstraints.FreezeRotationZ;
+        charCtrl = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Get button function
-        Button buttonJumpActive = buttonJump.GetComponent<Button>();
-        Button buttonReleaseActive = buttonRelease.GetComponent<Button>();
-        Button buttonPushActive = buttonRelease.GetComponent<Button>();
-        
+        //Button buttonJumpActive = buttonJump.GetComponent<Button>();
+        //Button buttonReleaseActive = buttonRelease.GetComponent<Button>();
+        //Button buttonPushActive = buttonRelease.GetComponent<Button>();
+
         //Jump Button
-        buttonJumpActive.onClick.AddListener(PlayerJump);
-        CountDown();
+        //buttonJumpActive.onClick.AddListener(PlayerJump);
+        //CountDown();
         PlayerRespawn();
     }
+
     void FixedUpdate()
     {
         //Get key input for player
         float y = CrossPlatformInputManager.GetAxis("Horizontal") * Time.deltaTime * 80.0f;
         float z = CrossPlatformInputManager.GetAxis("Vertical") * Time.deltaTime * 355.0f;
 
+
+        if (charCtrl.isGrounded)
+        {
+            // Player front and back movement
+            moveDirection = new Vector3(0, 0, z);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= moveSpeed;
+
+            jumpCount = jumpMax;
+        }
+        moveDirection.y -= gravityValue * Time.deltaTime;
+
         // Rotate & walking
         transform.Rotate(0, y, 0);
-        transform.Translate(0, 0, z);
+        //transform.Translate(0, 0, z);
+        Debug.Log("MoveDirection=" + moveDirection);
+        charCtrl.Move(moveDirection);
+
     }
-    void OnCollisionEnter(Collision collision)
-    {
-        // Jump reset when touch floor
-        var objectGround = collision.gameObject.name;
-        if (objectGround == "Floor")
-        {
-            jumpCount = jumpMax;
-            jumpingLimit = false;
-        }
-    }
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    // Jump reset when touch floor
+    //    var objectGround = collision.gameObject.name;
+    //    if (objectGround == "Floor")
+    //    {
+    //        jumpCount = jumpMax;
+    //        jumpingLimit = false;
+    //    }
+    //}
     public void OnTriggerEnter(Collider Boxes)
     {
         //Enable the push and release button when near the trigger
@@ -129,29 +153,35 @@ public class PlayerMovement : MonoBehaviour
         if (jumpCount >= 1)
         {
             //Player will move upward when press the jump button
-            playerRB.AddForce(transform.up * 50, ForceMode.VelocityChange);
+            //playerRB.AddForce(transform.up * 50, ForceMode.VelocityChange);
             jumpCount -= 1;
-            jumpingLimit = true;
+            //jumpingLimit = true;
+
+            moveDirection.y = jumpSpeed * doubleJumpMultiplier;
+            //Debug.Log("MOveDirection=" + moveDirection);
+            charCtrl.Move(moveDirection);
         }
     }
+
     // Jump Limit cooldown
-    void CountDown()
-    {
-        //Doing cooldown to prevent player spamming the jump button
-        if (jumpingLimit == true)
-        {
-            limitTime += Time.deltaTime;
-        }
-        if (limitTime > 15)
-        {
-            jumpCount++;
-            limitTime = 0;
-            jumpingLimit = false;
-        }
-    }
+    //public void CountDown()
+    //{
+    //    //doing cooldown to prevent player spamming the jump button
+    //    if (jumpingLimit == true)
+    //    {
+    //        limitTime += Time.deltaTime;
+    //    }
+    //    if (limitTime > 15)
+    //    {
+    //        jumpCount++;
+    //        limitTime = 0;
+    //        jumpingLimit = false;
+    //    }
+    //}
+
     //Player respawn position if they fall down to trap
     void PlayerRespawn()
     {
-        
+
     }
 }
