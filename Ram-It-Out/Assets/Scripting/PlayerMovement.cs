6f4c8bbe97d3a4ceb1de3d1de7be3912819da29c
William8500 <62640433+William8500@users.noreply.github.com>
2021-04-22 23:26:54 +0800

    William

--- Ram-It-Out/Assets/Scripting/PlayerMovement.cs
+++ Ram-It-Out/Assets/Scripting/PlayerMovement.cs
@@ -71,7 +71,7 @@ public class PlayerMovement : MonoBehaviour
 
         // Rotate & walking
         transform.Rotate(0, dirY, 0);
-        transform.Translate(0, 0, dirZ);
+        transform.Translate(-dirZ, 0, playerRB.transform.position.x);
     }
 
     void OnCollisionEnter(Collision collision)
